53aba3b6dbfa7f243b167a500a04ce74c03231d4
ErfAbkad6561 <erik656656@gmail.com>
2024-12-19 23:09:34 +0300

    Update interface.py
    визуализация для удобного взаимодействия с платформой, округлённые рамки, интерактивный курсор и ещё по мелочи

--- interface/interface.py
+++ interface/interface.py
@@ -1,9 +1,10 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import (
     QApplication, QMainWindow, QTableWidget, QTableWidgetItem,
-    QPushButton, QLabel, QLineEdit, QTextEdit, QComboBox
+    QPushButton, QLabel, QLineEdit, QTextEdit, QComboBox, 
 )
-from PyQt5.QtGui import QDoubleValidator, QIntValidator
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QCursor
+from PyQt5.QtCore import Qt
 import psycopg2
 import time
 
@@ -41,6 +42,7 @@ class DatabaseTableEditor(QMainWindow):
 
         self.create_filter_fields()  # Создаем элементы фильтров
         self.btn_filtr = self.create_button('Фильтр', 10, 235, self.loadTable_object_main)
+        self.btn_filtr.setCursor(QCursor(Qt.PointingHandCursor))  # Установить курсор для кнопки
 
         self.create_summarization_fields()  # Создаем элементы интерфейса для суммаризации
         self.table_rubrics = self.create_table_widget(10, 520, 290, 500)  # Таблица рубрик
@@ -83,6 +85,23 @@ class DatabaseTableEditor(QMainWindow):
 
         self.set_line_edit_validators()  # Устанавливаем валидаторы для ввода чисел
 
+        # Установка стилей для текстовых полей
+        self.setStyleSheet("""
+            QLineEdit {
+                border: 2px solid #ccc;                /* Светлая рамка */
+                border-radius: 5px;                    /* Округленные углы */
+                padding: 5px;                           /* Отступ внутри поля */
+                font-size: 12px;                        /* Размер шрифта */
+            }
+            QLineEdit:focus {
+                border: 2px solid #4CAF50;              /* Цвет рамки при фокусе */
+                background-color: #f9f9f9;              /* Фоновый цвет при фокусе */
+            }
+            QLabel {
+                margin-bottom: 5px;                     /* Отступ снизу */
+            }
+        """)  # Установка стилей
+
     def create_line_edit(self, x, y, label_text, width=290):
         """
         Создает текстовое поле с меткой.
@@ -106,9 +125,9 @@ class DatabaseTableEditor(QMainWindow):
         Создает элементы интерфейса для выполнения суммаризации.
         """
         self.label_usl_cod_model = QLabel('Модель:', self)
-        self.label_usl_cod_model.setGeometry(1200, 10, 140, 20)
+        self.label_usl_cod_model.setGeometry(1200, 15, 140, 20)
         self.label_dolya_usech = QLabel('Сжатие (%):', self)
-        self.label_dolya_usech.setGeometry(1350, 10, 140, 20)
+        self.label_dolya_usech.setGeometry(1350, 15, 140, 20)
 
         self.combo_box_usl_cod_model = QComboBox(self)  # Выпадающий список выбора модели
         self.combo_box_usl_cod_model.setGeometry(1200, 35, 140, 20)
@@ -119,9 +138,65 @@ class DatabaseTableEditor(QMainWindow):
         self.edit_dolya_usech.setValidator(QIntValidator(0, 100))  # Валидатор для целых чисел
 
         self.btn_summarization = self.create_button('Суммаризация', 1200, 60, self.get_summarization_result)
+        self.btn_summarization.setCursor(QCursor(Qt.PointingHandCursor))  # Установить курсор для кнопки
 
         self.edit_summarization_result = QTextEdit(self)  # Поле для вывода результата суммаризации
         self.edit_summarization_result.setGeometry(1200, 85, 290, 425)
+
+        # Устанавливаем стили для QTextEdit
+        self.edit_summarization_result.setStyleSheet("""
+            QTextEdit {
+                border: 2px solid #ccc;                /* Светлая рамка */
+                border-radius: 10px;                   /* Округленные углы */
+                padding: 0px;                          /* Отступ внутри поля */
+                font-size: 12px;                       /* Размер шрифта */
+                background-color: #f9f9f9;             /* Светлый фон */
+            }
+            QTextEdit:focus {
+                border: 2px solid #4CAF50;              /* Цвет рамки при фокусе */
+            }
+        """)
+
+        # Установка стилей для остальных полей
+        self.setStyleSheet("""
+            QLineEdit {
+                border: 2px solid #ccc;                /* Светлая рамка */
+                border-radius: 5px;                    /* Округленные углы */
+                padding: 1px;                           /* Отступ внутри поля */
+                font-size: 12px;                        /* Размер шрифта */
+            }
+            QLineEdit:focus {
+                border: 2px solid #4CAF50;              /* Цвет рамки при фокусе */
+                background-color: #f9f9f9;              /* Фоновый цвет при фокусе */
+            }
+            QLabel {
+                margin-bottom: 5px;                     /* Отступ снизу */
+            }
+            QComboBox {
+                border: 2px solid #ccc;                 /* Светлая рамка для комбобокса */
+                border-radius: 5px;                     /* Округленные углы */
+                padding: 0px 5px;                       /* Отступ внутри */
+                font-size: 12px;                        /* Размер шрифта */
+            }
+            QComboBox:focus {
+                border: 2px solid #4CAF50;              /* Цвет рамки при фокусе */
+            }
+            QPushButton { /* Стили для кнопки */
+            background-color: white; /* Зеленый цвет фона */
+            color: black;              /* Белый цвет текста */
+            border: 2px solid #ccc;     /* Светлая рамка */
+            border-radius: 5px;         /* Округленные углы */
+            padding: -5px 10px;         /* Отступы внутри кнопки */
+            font-size: 12px;            /* Размер шрифта */
+            font-weight: none;          /* Жирный шрифт */
+            }
+            QPushButton:hover { /* Стили при наведении курсора */
+                background-color: #45a049; /* Темнее зеленый цвет при наведении */
+            }
+            QPushButton:pressed { /* Стили при нажатии */
+                background-color: #3e8e41; /* Еще темнее зеленый цвет при нажатии */
+            }
+        """)  # Установка стилей
 
     def load_initial_data(self):
         """
